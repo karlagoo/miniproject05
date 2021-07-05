28265a20b89179babc4c36cb71a705a281efdead
Karla Goo <karla_goo@yahoo.com>
2021-07-05 18:19:24 -0500

    added initial vars

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log("Script attached")
+const projectNameInput = $("#project-name")
+const projectTypeSelect = $("#project-type")
+const hourlyRateInput = $("#hourly-rate")
+const dueDate = $("#due-date")
+
+$("#submit-button").on("click", function(event){
+    console.log("submit project")
+}
+)
